46775397528a476f4c1ae5b9b0f245d9645d05b6
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-05 17:38:18 +0545

    Update python.py

--- python.py
+++ python.py
@@ -3,6 +3,105 @@ print("age")
 print(age)
 print(str(age))
 
+# "string indexing",
+
+a='shuvam'
+print(a[1])
+
+# What is the output of print(a[0]) if a = 'hello'?
+a='hello'
+print(a[0])
+#If s = 'python', what does s[2] return?
+s='python'
+print(s[2])
+
+#Given name = 'alice', what is the result of name[4]?
+name='alice'
+print(name[4])
+# What does print(word[-1]) output if word = 'world'?
+word='world'
+print(word[-1])
+# If text = 'programming', what is text[5]?
+text='programming'
+print(text[5])
+# Given s = 'abcdef', what is s[3]?
+s='abcdef'
+print(s[3])
+# What is the output of print(a[2]) if a = 'chocolate'?
+a='chocolate'
+print(a[2])
+# If str = 'elephant', what does str[0] return?
+str='elephant'
+print(str[0])
+# Given x = 'banana', what is x[-2]?
+x='banana'
+print(x[-2])
+# What does print(s[1]) output if s = 'computer'?
+s='computer'
+print(s[1])
+# If a = 'butterfly', what is a[6]?
+a='butterfly'
+print(a[6])
+# Given name = 'alexander', what is name[5]?
+name='alexander'
+print(name[5])
+# What is the output of print(word[3]) if word = 'dictionary'?
+word='dictinary'
+print(word[3])
+# If s = 'eleven', what does s[-3] return?
+s='eleven'
+print(s[-3])
+# Given text = 'javascript', what is text[4]?
+text='javascript'
+print(text[4])
+
+# Given s = "programming", what is s[-4]?
+s="programming"
+print(s[-4])
+# If s = "abcdefghij", what does s[::2] return?
+s="abcdefghij"
+print(s[::2])
+# What is the output of print(s[1:-1:2]) if s = "hello world"?
+s="hello world"
+print(s[1:-1:2])
+
+# Given s = "python", what does s[::-1] produce?
+s="python"
+print(s[::-1])
+# If s = "123456789", what is s[2:7:2]?
+s="123456789"
+print(s[2:7:2])
+# What does s[-5:-1] return if s = "abcdefghijklmnop"?
+s="abcdefghijklmnop"
+print(s[-5:-1])
+# Given s = "slicing", what is s[1:6:3]?
+s="slicing"
+print(s[1:6:3])
+# If s = "abcdef", what is s[-3:-6:-1]?
+s="abcdef"
+print(s[-3:-6:-1])
+# What is the output of print(s[5:1:-1]) if s = "education"?
+s="education"
+print(s[5:1:-1])
+# Given s = "algorithm", what does s[3:-3] return?
+s="algorithm"
+print(s[3:-3])
+# If s = "mississippi", what is s[2:9:3]?
+s='mississippi'
+print(s[2:9:3])
+# What does s[-1:-6:-2] produce if s = "abcdefgh"?
+s="abcdefgh"
+print(s[-1:6:-2])
+# Given s = "reverse", what is s[6:0:-2]?
+s="reverse"
+print(s[6:0:-2])
+# If s = "1234567890", what does s[-2:2:-2] return?
+s="1234567890"
+print(s[-2:2:-2])
+# What is the output of print(s[3:0:-1]) if s = "dynamic"?
+s="dynamic"
+print(s[3:0:1])
+
 
 
 
