963f816c0c3aa66eb4e691d1b745c4b0665efce3
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-11 19:40:46 +0545

    Update string&conditional.py

--- string&conditional.py
+++ string&conditional.py
@@ -49,7 +49,105 @@ print(final_str)
  #i will continue tommorow for sure///
  #LET'S GOOOOOO FOR BETTER FUTURE
  
- 
+ # #1...string &conditional statements
+# thing='Hi my name is shuvam adhikari. \n i am from hetauda .\n i am doing great/n what"s up'
+# print(thing)
+# print(len(thing)) # yo syntax le length nikalca
+
+#2..index  (0=s,1=h,2=u,3=v,4=a,5=m etc like this....)
+name='shuvam adhikari'
+print(name[2])
+#3..
+number=1,2,3,4,5,6,7,8,
+print(number[4])
+
+#4...
+animal=('hi my name is animal i am doing nothing but eating sleeping jumping ')
+print(animal[2],animal[4],animal[6])
+
+#5...SLICING INDEX
+name ='shuvam adhikari'
+print(name[0:6])  #YO GARDA JASTAI (0=s,1=h,2=u,3=v,4=a,5=m )0:6 
+
+#6..NEGATIVE INDEX (-5,-4,-3,-2,-1) apple
+fruit=' apple '
+print(fruit[-6:-1])
+
+name=' shuvam '
+print(name[-7:-1])
+
+
+#7...STRING FUNCTION SYNTAX print(somthing.endswith('one'))
+somthing=' i am doing somthing on my one'
+print(somthing.endswith('one'))
+
+#...
+full_name=' shuvam adhikari '
+print(full_name.endswith('ira')) # false aayo kina vane adhikari ko end 'ari' bata vako cha 
+
+#..
+lab=' i am doing somthing on my laptop'
+print(lab.endswith('top'))
+
+
+#8...print(somthing.capitalize(' '))
+name='shuvam adhikari'
+print(name.capitalize())
+
+#..
+chr='masterpiece'
+print(chr.capitalize())
+
+#9..print(chr.replace('name ',to change name' '))
+chr=' shuvam adhikari '
+print(chr.replace('shuvam','kriti'))
+
+#..
+name='shuvam adhikari'
+print(name.replace('shuvam adhikari','laxman thapa'))
+
+name=' ram adhikari '
+print(name.replace('ram','laxman'))
+
+#10 (find function)
+
+character=' prakriti adhikari'
+print(character.find('kr'))
+
+#..
+name='shuvam adhikari'
+print(name.find('h'))
+
+table='daita raj'
+print(table.find('j'))
+
+#11...print(table.count('')) 
+# ( YO LE CHAI KATI WATA CHA VANE RE COUNT GARNE KAM GARCHA )
+
+name=' shuvam adhikari @ gmail.com '
+print(name.count('a'))
+
+chr=' a,b,c,d,e,f,g,h,a,c,v,f,a,s,d,f,h,e,e,e,e,e,e,e,e,e,e,e,e,e,e,c,v,d,a,a,e,e,r,e,r,f,d,f,x,z,v,'
+print(chr.count('e'))
+
+
+#REPLACE
+name=' shuvam adhikari '
+print(name.replace('shuvam','kriti '))
+
+
+#practice questions 
+#1...
+name=input('name:')
+print(len('name'))
+
+#2..
+chr=' paisa koo sign $,$,$,$ ho k ho $ ho k cha re $ cha ho good'
+print(chr.count('$'))
+
+
+
+
  
  
  
