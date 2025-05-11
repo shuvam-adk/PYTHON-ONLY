93eb6c62bb9b1c8fd81ff83164b236a19d739b66
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-11 16:58:53 +0545

    Update ("IF-ELIF-ELSE").py

--- ("IF-ELIF-ELSE").py
+++ ("IF-ELIF-ELSE").py
@@ -226,6 +226,36 @@ else:
     print('error')
 
 
+
+
+
+marks=85
+if marks>=90:
+  print("Grade A")
+elif marks >=80:  # Cheks if marks are 80 or above  BUT less than 90
+  print("Grade B") 
+else:
+  print("Grade C")
+   
+
+
+   #WHAT IS THE MISTAKEN ON THIS CODE FINDOUT LATER
+   #LEARNING DAY 4.0
+num1=int(input("5:"))
+num2=int(input("5:"))
+sum=num1+num2
+print("Sum:",sum)
+
+
+marks=80
+if(marks>=90):
+    grade= "A"
+elif(marks >=80 and marks < 90):
+    grade="B"
+elif(marks >=70 and marks < 80):
+    grade="C"    
+
+
 #no...---
 food=input("food:")
 eat="yes" if food=="cake"else "no"
