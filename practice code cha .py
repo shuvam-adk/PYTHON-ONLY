57425d90a14df61734335ff67cc887605543f195
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-13 17:14:12 +0545

    Update and rename Empty cha .py to practice code cha .py

---
+++ practice code cha .py
@@ -0,0 +1,146 @@
+# 1. Write a program to print "Hello, World!"
+print('hello,world')
+
+# 2. Write a program that asks for your name and greets you
+full_name="Shuvam Adhikari"
+greets="Nameste"
+print(f"my name is {full_name}, & {greets}")
+
+# 3. Write a program that calculates the sum of two numbers
+num1=10
+num2=21
+sum=(num1+num2)
+print(sum)
+print(f'the value of the sum is {sum}')
+
+# 4. Write a program to swap two variables
+a=10
+b=50
+b=a
+print(b)
+
+#...
+
+light=input('trafic light:')
+if light== 'green':
+    print('go')
+elif light=='yellow':
+    print('look')
+elif light == 'red':
+    print('stop')
+else:
+    print('nepal government is worthless')
+     
+#122222
+marks = int(input('marks:'))
+if marks >=90:
+    print('A+')
+elif marks >=80 <89:
+    print('A')
+elif marks >70 <=79:
+    print('B+')
+elif marks >60 <=69:
+    print('B')
+elif marks >50 <=59:
+    print('c+')
+elif marks >39 <=49:
+    print('just pass')
+else:
+    print('fail')
+
+
+#...
+light = input('treffic light :')
+if light == 'red':
+    print( 'stop ')
+elif light == 'yellow':
+    print( 'look ')
+elif light == 'green':
+    print( ' you can go ')
+else:
+    print(' you cant go anywere')
+    
+#..
+
+
+marks=85
+if marks>=90:
+  print("Grade A")
+elif marks >=80:  # Cheks if marks are 80 or above  BUT less than 90
+  print("Grade B") 
+else:
+  print("Grade C")
+   
+
+
+   #LEARNING DAY 4.0
+num1=int(input("5:"))
+num2=int(input("5:"))
+sum=num1+num2
+print("Sum:",sum)
+
+
+marks=80
+if(marks>=90):
+    grade= "A"
+elif(marks >=80 and marks < 90):
+    grade="B"
+elif(marks >=70 and marks < 80):
+    grade="C"    
+
+
+#...
+
+#1.. count the  'o'...
+chr='i am the person of the oooo'
+print(chr.count('o'))
+
+#2.. IF STATEMENT
+age=int(input('the age of:'))
+if age >=22:
+   print('he can have a bike licence ')
+elif age <21:
+    print('he cant have licence')
+else:
+    print(' political back ground ')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
