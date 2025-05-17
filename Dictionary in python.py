d484aa01d4a723d0040eb73cb5bf0b7b4e0f5c52
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-17 17:25:33 +0545

    Update Dictionary in python.py

--- Dictionary in python.py
+++ Dictionary in python.py
@@ -1,4 +1,56 @@
 #Dictionary in python...
+
+
+
+# # ##1....my dict.key ()  , #myDict.['name']) , #(myDict.value()) ,,
+##  #(myDict.item()) ,,  #(myDict.get('key'))
+
+father = {
+    'name': 'rohon' ,
+    'reason of death': 'bike accident', 
+        'children': 3,
+            'daughters': {
+                'older':'prakriti',
+                'younger':'swikriti', 
+                },
+                'son': {
+                'name':'rawan'  
+        }
+        
+    }
+print(father)
+print(father['name'])           ##myDict.['name'])
+print(len(father.keys()))      # #(myDict.key())
+print((father.values()))        ##(myDict.value())
+print(father.items())           ##(myDict.item())
+print(father.get('daughters'))    # #(myDict.get('key'))
+father.update ({'city':'kathmandu' })  ## myDict.update({ '...' : '...'}) 
+print(father)
+
+
+#2.... 
+chr= {
+    'name':'shuvam',
+    'work':'unemployed',
+    'age': '22',
+    'hobby':{
+        'game':['footbal','cricket','video game'],
+        'another hobby':{
+            'sketch':'animated drawing',
+            'from':'hetaude',
+            
+        }
+    }
+    
+}
+print(chr)
+print(type(chr))
+print(chr['age'])
+print(chr['work'])
+print(chr['name'])
+print(chr['hobby'])
+
+
 #1...
 str='shuvam adhikari'
 chr=str[6:15]
