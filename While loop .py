5d22fb3e6a171966ea4b2122b609f20856ef9f2d
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-22 20:34:34 +0545

    Update While loop .py

--- While loop .py
+++ While loop .py
@@ -1,6 +1,43 @@
 
 # ##while loop
+###
+q=1
+w=1
+e=1
+o=1
+while q<=5 :
+    while w<= 4 :
+      while e<=3 :
+        while o<=3 :
+          print('shuvam')
+          print('adk')
+          print('ambi')
+          print('raja')
+          o+=1
+        e+=1
+      w+=1
+    q+=1
 
+#####....
+name=input('enter your name :')
+age=(input('enter  age :'))
+location=input('location :')
+while name == '':
+  print('you did not enter  your name')
+  name=input('enter your name :')
+print(f'hello {name}')
+while age == '':
+  print('you didnot enter your age:')
+  age=(input('enter your age :'))
+  age=int(age)
+print(f'your age is {age}')
+while location=='':
+  print('you have to locate your place as well')
+  location=input('location :')
+print(f'your location is {location}')          
+
+ 
+    
 #1.
 i=1
 while i<= 6 :
