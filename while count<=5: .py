13d9dbeafccd5ebebbefa895ad2b0a6a3a2f9bb8
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-24 21:13:16 +0545

    Update and rename while count<=5: to while count<=5: .py

---
+++ while count<=5: .py
@@ -0,0 +1,25 @@
+##..
+name=input('enter youre name:')
+job=input('entr your jobe name :')
+salary=int(input('enter your salary :'))
+age=int(input('enter ytour age :'))
+
+dict={
+    'name':name,
+    'job':job,
+    'salary':salary,
+    'age':age,
+    
+}
+print(name,job,salary,age)
+print(dict)
+
+
+count=1
+while count<=5:
+    print('hello') 
+
+#Day3 of some extra coding for better performence in coding line 
+age = 18
+if age >= 18:
+    print("You can vote")
