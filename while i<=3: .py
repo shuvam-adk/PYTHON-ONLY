bf3e4d0baa3a080b1237dc0863f5bcc4cff5be1d
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-24 20:56:45 +0545

    Update while i<=3: .py

--- while i<=3: .py
+++ while i<=3: .py
@@ -51,13 +51,3 @@ while u<=3:
 
 
 
-
-
-marks={}
-x=int(input('enter phy: '))
-marks.update({'phy': x})
-x=int(input('enter math: '))
-marks.update({'math': x})
-x+int(input('enter chem: '))
-marks.update({'chem': x})
-print(marks)
