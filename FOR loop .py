100c0acea3e4087fb1189d151dffde234538cf89
Shuvam Adhikkari <adhikarishuvam27@gmail.com>
2025-05-27 20:13:53 +0545

    Update and rename empty cha.py to FOR loop .py

---
+++ FOR loop .py
@@ -0,0 +1,51 @@
+# FOR lOOP= IT EXECUTE A BLOCK OF CODE A FIXED UMBER OF TIME YOU CANE ITERATE OVER A RANGE, STRING,SEQUENCE ETC 
+
+##1..count 1 to 10
+for x in range (1,11):  # it will avoid last 11 and print upto 10 .
+    print(x)
+    
+##2.. happybirthday
+for x in range(11,0,-1):
+    print(x)
+print('happybirthday')
+
+## credit card number
+card='1234-5678-9012'
+for x in card:
+    print(x)
+
+#
+name='a','s','s','d','f'
+for x in name:
+    print(x)
+
+###
+for x in range(1,21):
+    if x==12:
+        continue
+    else:
+        print(x)
+
+for x in range (1,21):
+    if x==10:
+      continue
+    else:
+      print(x)
+
+
+## 18 ra 19 lai skip gare ra print  gare ko 50 sama
+for x in range (1,50):
+    if x==18 or x==19 :
+      continue
+    else:
+       print(x)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
